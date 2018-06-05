2368073baaa9506f0474a1aada01cc9c57215ca7
Matthew Burke <matthewpatrickburke@gmail.com>
2018-06-05 14:48:36 -0600

    getter and setter for HTML

--- renderer.js
+++ renderer.js
@@ -1,5 +1,16 @@
 var ipcRenderer = require(`electron`).ipcRenderer;
 
+function getHTML() {
+	var body =  document.body.innerHTML;
+	var ed = document.getElementById('ed');
+	ed.value = body; 
+}
+
+function setHTML() {
+	var newHTML = document.getElementById('ed').value;
+	document.body.innerHTML = newHTML;
+}
+
 function executeArea() {
 	var ed = document.getElementById('ed');
 	eval(ed.value);
